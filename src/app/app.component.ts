c4c8cd738dba097854ed216a2284faac0042a93d
AmiraAHady <amirasgithub@gmail.com>
2024-09-08 20:20:28 +0300

    day 1-angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { FooterComponent } from './components/footer/footer.component';
+//  Decorator
 @Component({
+  // component directive
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NavbarComponent,HomeComponent,MoviesComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
+// component
 export class AppComponent {
-  title = 'day1';
+  title:string = 'day1';
 }
+
