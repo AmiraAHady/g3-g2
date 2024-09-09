fda9a59b1f506bf78b4fefe8ad67aa6462bc2640
AmiraAHady <amirasgithub@gmail.com>
2024-09-09 20:02:33 +0300

    day 2-angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { MoviesComponent } from './components/movies/movies.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { TvListComponent } from './components/tv-list/tv-list.component';
 //  Decorator
 @Component({
   // component directive
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent,HomeComponent,MoviesComponent,FooterComponent],
+  imports: [NavbarComponent,HomeComponent,MoviesComponent,FooterComponent,TvListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
+  // component directive
   selector: 'app-movies',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './movies.component.html',
-  styleUrl: './movies.component.css'
+  styleUrl: './movies.component.css',
 })
 export class MoviesComponent {
+  searchMovie:string='data'
+  movieDetail:any = {
+    title: 'Spider-Man Unlimited ',
+    image:
+      'https://media.themoviedb.org/t/p/w600_and_h900_bestv2/fvmC5QzQwkI5ZNJauE4xpF24PTB.jpg',
+    description:
+      'Spider-Man travels to Counter-Earth to rescue a Terran shuttle crew trapped there and discovers a tyrannical & warped version of his world.',
+    isAvaliable:false  
+  };
 
+  watchMovie(title:string){
+    console.log(`user want to watch ${title}`);
+    
+  }
 }
